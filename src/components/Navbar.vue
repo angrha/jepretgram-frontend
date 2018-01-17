a8f09832e156c6a3197098f3955c160d1ba3b224
angrha <angrha@gmail.com>
2018-01-17 21:18:57 +0700

    no vuex login

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <router-link to="/" class="nav-link"> <span class="sr-only">(current)</span></router-link>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Features</a>
